67d706f78468d2f57bf7812f1a4b7cbe0b2e4523
jkutkut <jorgeregonzalez@gmail.com>
2021-05-27 18:21:32 +0200

    updated

--- testing/oneLine/smallOneLine.js
+++ testing/oneLine/smallOneLine.js
@@ -1,1 +1,1 @@
-let h=["hola, que tal estás? + - == ","hey + 2",{hos:1+2,yha:2+("1 + 2"-2**2/2+1)}];console.log(h);
+let h=["hola, que { ; tal estás? + - == ","hey + 2",{hos:1+2,yha:2+("1 + 2"-2**2/2+1)}];console.log(h);
