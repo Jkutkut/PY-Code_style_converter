172776f9178c4797d70b75723aa086193bf568f6
jkutkut <jorgeregonzalez@gmail.com>
2021-05-27 15:10:25 +0200

    JS file to one-line JS file created

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
+import re # Regular expresions
+
 inputFileName = "testing/input.js"
 outputFileName = "outputFile.js"
 
 inputFile = open(inputFileName, "r").read().split("\n")
+outputFile = open(outputFileName, "w")
+
+output=""
 
 
 for i in inputFile:
+    i = re.sub(r'^ +', '', i) # Remove initial spacing
+    i = re.sub(r'//.+', '', i) # Remove one line comments
     print(i)
+    output += i
+
+output = re.sub(r'/\*\*.+\*/', '', output)
 
 
-outputFile = open(outputFileName, "w")
-
-
-
+outputFile.write(output)
 outputFile.close()
