aa609bc1a87787130577868b5720e78ca3126285
jkutkut <jorgeregonzalez@gmail.com>
2021-05-27 23:50:16 +0200

    added

---
+++ testing/normal/functionalTest_Normal.js
@@ -0,0 +1,11 @@
+let input1 = "the numbers are: ";
+let input2 = "0, 1, 2, 3, 4, 5, 6, 7, 8, 9\n";
+
+let obj1ArR = [input1, input2];
+
+function mergerFunction(text) {
+    let result = text.join("");
+}
+
+console.log(mergerFunction(obj1ArR));
+
